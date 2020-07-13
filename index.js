c7485271d76857da23581a3ea69fd12f4bdd60c6
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-12 20:14:34 -0400

    fix index

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var url = "https://github.com/Jack5079";
-var start =
-  (process.platform == "darwin"
-    ? "open"
-    : process.platform == "win32"
-    ? "start"
-    : "xdg-open");
-require("child_process").exec(start + " " + url);
+require('open')('https://github.com/Jack5079/') // Jack5079
