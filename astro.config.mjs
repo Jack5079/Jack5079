fbe0120594580d29e53695e71df3d3ff538c5403
Jack W <jack-git@cetera.uk>
2023-03-10 22:21:20 -0500

    feat: :technologist: Svelte support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 
-import sitemap from '@astrojs/sitemap'
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://j.cetera.uk',
-	integrations: [mdx(), sitemap()],
-	output: 'static',
+	site: "https://j.cetera.uk",
+	integrations: [mdx(), sitemap(), svelte()],
+	output: "static",
 })
