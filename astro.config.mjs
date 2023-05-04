a5d37ba102b7cbeb8a69561b8876c14d8d10e8e7
Jack W <jack-git@cetera.uk>
2023-05-04 15:48:23 -0400

    feat: :speech_balloon: SNIPE OF A LIFETIME LMAOOOOOOO

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://j.cetera.uk",
+	site: "https://jack.cab",
 	integrations: [mdx(), sitemap(), svelte()],
 	output: "static",
 })
