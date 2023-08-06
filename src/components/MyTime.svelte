e36e20bec86a0273a678ea65a1523a0e39866050
Jack W <git@jack.cab>
2023-08-06 08:04:27 -0400

    feat: :sparkles: Add my timezone to the list of socials
    Ideally without the link but idc, also need to figure out the best way to make this accessible

---
+++ src/components/MyTime.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { onMount } from "svelte"
+
+	let browser = false
+	let time
+	onMount(() => {
+		time = new Date().toLocaleString("en-US", {
+			timeZone: "America/New_York",
+			hour12: true,
+			hour: "numeric",
+			minute: "numeric",
+		})
+		const id = setInterval(() => {
+			time = new Date().toLocaleString("en-US", {
+				timeZone: "America/New_York",
+				hour12: true,
+				hour: "numeric",
+				minute: "numeric",
+			})
+		}, 1000)
+		browser = true
+		return () => clearInterval(id)
+	})
+</script>
+
+<span>
+	{#if browser}
+		({time})
+	{/if}
+</span>
