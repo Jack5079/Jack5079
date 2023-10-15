d3ad63490947d7561eb4d9ff84f64fdac337a4d2
Jack W. <git@jack.cab>
2023-10-14 21:48:07 -0400

    perf: :zap: Replace MyTime with vanilla JS
    No more Svelte!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 
-import svelte from "@astrojs/svelte"
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://jack.cab",
-	integrations: [mdx(), sitemap(), svelte()],
+	integrations: [mdx(), sitemap()],
 	output: "static",
 })
