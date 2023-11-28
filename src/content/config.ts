18367ff1cc2ca5a511d4ea6b954090760833ddcf
Jack W. <git@jack.cab>
2023-11-28 18:22:33 -0500

    perf: :zap: Responsive images on blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,29 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content"
 
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string().optional(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
-});
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string().optional(),
+			// Transform string to Date object
+			pubDate: z
+				.string()
+				.or(z.date())
+				.transform(val => new Date(val)),
+			updatedDate: z
+				.string()
+				.optional()
+				.transform(str => (str ? new Date(str) : undefined)),
+			image: z
+				.tuple([
+					image(),
+					z.string({
+						description: "Alt text for image",
+					}),
+				])
+				.optional(),
+		}),
+})
 
-export const collections = { blog };
+export const collections = { blog }
