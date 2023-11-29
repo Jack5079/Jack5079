170cfce3162608fbcfc5d63fe3f14cccc77f8d54
Jack W. <git@jack.cab>
2023-11-28 23:57:01 -0500

    refactor: :speech_balloon: Change site description to :wavesmiley:

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jack W.';
-export const SITE_DESCRIPTION = 'hi im jack';
+export const SITE_DESCRIPTION = ':wavesmiley:';
