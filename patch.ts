526865d2423a570b5009062f4f60ba4ea7871531
Jack W. <git@jack.cab>
2024-04-14 15:21:52 -0400

    fix: I swear to god if this doesn't fix it

---
+++ patch.ts
@@ -0,0 +1,62 @@
+/*!!^yF\\\/{\|||7?|\,'^@@Q:,!!!/Kmi\\\\!'''''.-```'',_::!?oNQQQQQQ#0##DKKRQQQQ#gg#QQQ
+^^;;;+4v\\/ix|||/l*\>',f@@D|!!!!oKxi/\\r,''''''',~::!!\odQ@@QQQQNg0d9qmPmegB#ggNNNQQQ
+rr^^;;|wl\\\sv|||v\?i!,tQ@@%|!!!*Owi\|=^:,,~_:::::!>yHQQQ@@@QQBg%OdkyZu}uj9%R%%RN#QQQ
+++r^^^^(f|=\\Ii||\s|\/:!^\^!i!:::jU\r=\7i\/i?*r^+/zZeZX6qbNQQQQgdK6h6XZfx{xoZ69KKRR#Q
+>>++r^^^/z^|\/7|||\||/+'....,:~:!{bqkmyyfsiilv(iiv(lll7iiv7sfoEb%gNN%q9mov{IS4e4k99Kd
+\||=>+rr>Pv///\\\|||||l!.',,:;(POqkeussxvli|*??|||\|\\|\\|*??||?|if4Kqkm5sz}j9KKbqkPX
+/???*>>++i9vi//\\|||\|/i^!>ljhkq66oxvliii||*rr>/\|??|\7zv|>+>?||?>*|/|?|/(iiu4XqHR%6E
+??|*r+*=>>jUl///s/||(s\vsxawXXwk6moIsv|r^!;;;!r|=*|\is4weus(x/|\/i\\/r!!:!!>\zuShb%K6
+;^^^!!=|??|X4i/ltj\\/fvsX6UqOKqk6euzvv\r!!::_:!r+|/(IE%g8ha5x/ii\?**>+r;:,,,_!^\(vljX
+:!!!!!^?|||\duiivytitk0###N8q9bbqmFvvzs/|^!:,:^*i{jZ9##N9jIt\\|\\\/li\|>;:,''',:!::^i
+!!!!!!!;|\\\sRtilzEEObPyuoZ6PomKqwf(vxsxi?^:~!*lIoPdg8%KZi*?^!^>>\iiiivli*:.```''~::!
+!:::!;!;^/ilsqDvi(i?^!!!:!>IIFa4E4}zzfuvv\+!:>(ymkOg%qwt\^_:::!!!;r^^r*|il\!-   ``-,,
+',,:!!::!xqOOd#m*~,.':!::,,!+|s5PexSooouts\r+vokD6bqEy{\!:,,''''''''.''',:^?!-    ``'
+','',_!!:!U%%%%W('`',,'',!*!:!|edqjP9k4jfu/\I4OdwXmoyv|!,'....-```       `.:;~`      
+,,'.':!!:,|%8Hkkm!'/e/,'':!^!;l6Hbb%qXoojjxokkP6Kqofu\!,'.'.`      ````    -_!-      
+:~:!!!!!!:,\Zu(/j*!^^:::::~^^I6OgBN%k4XUP4ehEk9kXZIszi^:_''.--..`` ``` ``  `,:-      
+i',~:~~:::,,!!::i7|\=^^rr^?|I6gQQNKX4Z4omEXeXwXZojFvivsi>;:.```-.'''',.`--.',.`      
+eu!',:,,,,'',:_:;jojss}ilizy6#QQg64ooSSmkXyjSaoo4ayuIvii|+^!,,'''.-``-'---.:,`       
+\>;_',,_~:!:_,,~:{qqwmyfj46%QBWK4tsuo4XEXZyffoooZZ4ZjIs(ii|r^,!::~,,''.-.':!:`       
+;;!!r!'',,,''',~:\{%NNg8R%N#R6eyfujamP9qhojFIfFyaoaoyjI{xvv/|>^;!::,,~_~:!+^:`       
+!!::~,'-.'''''',,!laQQQQN#gKeSjjoZkOdO6waZ4ojIFfy44ojjI{II}}v/i\|^;!!!;^+|\>!'       
+::::_~,'''',,'',,~*\OQQQ#WEuvlvtjaUOKkZSjfujSoSjSaZo5yI}uyjFI7(\i/*+++>|il|r!.       
+::::~~~''''',,,,,~:|a#QB%P5{vi\ivoXEX5fuFt}jamea4ewXw4a5jjyjFtuyfv|**=*\\\*+:`       
+:~,,,,,'''',,~::;(UgBNbmyIz(i/ilvxxsvi7vzFya4aooZwk6Peaoyf}}uyjjjI\>+|//\|*r,        
+:_~,,,''..-.',ryKgqol|+^r>||||\\\\\|*rrr*\vFftvv{yek9heoyfujyuFufti\?||\|r^;'        
+::^!,.```-.':i9dj>:~,,,,,,~~,~~~~~~_::::!;+|\\/lvsFoPhPeZyfFIIxFjftz|r^^r!::`        
+``-`-``-',:*E6{!:,,,,'''''''''..''''''',,,::!r?i{FfuSm66moSF}xsi7xsv\!,_~'-`         
+``--..--':z6ur:~,,,,'''.....----...'''''''',,:;\vs{ujS4kkZj}xs7li(ii>:,''`           
+.''-`--';Zkl!::~,,,,'''''..-------.'''''''''',~;\iiv}FFSZw4usvvi///\:,,,'`           
+.--.'',=oo|r^!!:~,,,'..'''.--````-.'''''''''''',;||/szxIyae4fi/\\\/^,,,'-            
+'''.-.~sj|**=+^;!_,''-.''''.````````--.'''''...',;*\(7(vxja4ov\||?!.-'.-`            
+--```'?mi!:~~~::~,'.--.'''.-``       ````---```.,!|\ii//(suoS{l/|=.`````             
+````-:}e+,``  ``-'.````-..-``            ``````-,!=\il(vsIjyyuts>'-```               
+````.+oX|'`      `.`````'.``                ```'_^*\\/vvsIFjoy}x;.-``                
+'..',=oUm|'`      `.`` `'-`                  `-~!^>?|/iilxuSyFIF+` ```               
+```-'>4U6wi^!'` ``.'-` ```                  ``,:!;^>|i7l(xfujIIt/,``````````         
+````,+j96Pji\=:::!!,-`  ``                 `.,:::!!^?*|ivst}FII7>,`````````````````.,
+...''!vkqPoI>!!::!:,`    ``              ``',~:::!^+||\(vs{}t{t/:.````````````.'''.-.
+''..':+o964j(^,''''.``   `-`           `-.'',~::!^^r>>/vsssvstv>'````````-',,,,,,,,'-
+'-`,,,:iakwoIi+~.``````  ``````       `-'',,~:::!;^>\iivsvvvvvl!``````-,!!:_,,~,,,,'-
+.`.,,~:^veXauvi?^:'.````  ``````    `-',,,,,~::!!;^+\iiiiiiii/|~````   `.~:''-.-`````
+``---.',!iaZSxi\?r;!:'-`` `````--..',~:!:!:::!!;^r+?|\///|r^!:_.``       `--'````````
+```````-';\tjus/|>r!:,''','.''.'',,,_::!!!!!!;;;^r*||?>+r:,'```` ``       ',,...--```
+`````````,:^|lssi|?*r!!!!^;!::~~,,,,~:::!^^!;^r^^^+=?*^:'-`````````` `    -:,-```   `
+`````````-,'':;|r|/ii\|^r+;;r>>r^;^!!!!!;^^^rrr;;^!!:,.````````````       .'`        
+``````````.''',,',,:;^r^^;:::!!^>>**>*=r=*+r^r^:,'.-````````` `                     `
+```````````-','-`` ``',:!!!!^^^+??=***>;^^:~~~,-`` ``` `````                      ` `
+```````````..``````  ```--'',,::!!!:::~',,``-''```  ``                               
+'''''''',,:~- `   `        `` ``-````````````'-``                                    
+,,''',,~_~,'                         ```  ```.-.`                              `   */
+
+import { file } from "bun"
+const fyle = file(
+	`${import.meta.dirname}/node_modules/@marko/run-adapter-static/dist/index.js`,
+)
+await Bun.write(
+	fyle,
+	(await fyle.text()).replace(
+		`htmlparser2/lib/WritableStream"`,
+		`htmlparser2/lib/WritableStream.js"`,
+	),
+)
