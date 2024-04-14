3ded20372a1365d44a5173670e0876d209c34b30
Jack W. <git@jack.cab>
2024-04-14 15:01:41 -0400

    fix: add marko to vite config so that it can actually build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite"
 import { Mode, plugin as markdown } from "vite-plugin-markdown"
+import marko from "@marko/run/vite"
+import staticAdapter from "@marko/run-adapter-static"
 
 export default defineConfig({
-	// plugins: [marko({ adapter: nodeAdapter() })]
 	plugins: [
+		marko({ adapter: staticAdapter() }),
 		markdown({
 			mode: [Mode.HTML],
 		}),
