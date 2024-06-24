ff930d133c606233fc42b40279ad3774906fee8f
Jack W. <git@jack.cab>
2024-06-24 14:20:24 -0400

    fix: Remove unused markdown plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite"
-import { Mode, plugin as markdown } from "vite-plugin-markdown"
 import marko from "@marko/run/vite"
 import staticAdapter from "@marko/run-adapter-static"
 
 export default defineConfig({
 	plugins: [
-		marko({ adapter: staticAdapter() }),
-		markdown({
-			mode: [Mode.HTML],
-		}),
+		marko({ adapter: staticAdapter() })
 	],
 })
