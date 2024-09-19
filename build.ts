0829e446c9c3adcb63ed2049cb715de78349718b
Jack W. <git@jack.cab>
2024-09-19 00:50:05 -0400

    perf: Actually subset the font

---
+++ build.ts
@@ -0,0 +1,21 @@
+import { $, Glob } from "bun"
+import { readFile, writeFile } from "fs/promises"
+import subsetFont from "subset-font"
+import quotes from "./src/quotes.json"
+
+await $`marko-run build`
+await $`cp -r ./public/. ./dist`
+await $`mv ./dist/404/index.html ./dist/404.html`
+await $`rmdir ./dist/404`
+
+const fonts = new Glob("dancing-script-*.woff*")
+
+for await (const file of fonts.scan("./dist/assets/")) {
+	writeFile(
+		`./dist/assets/${file}`,
+		await subsetFont(
+			await readFile(`./dist/assets/${file}`),
+			quotes.join("\n"),
+		),
+	)
+}
