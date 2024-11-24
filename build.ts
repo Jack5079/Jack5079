9c7b7c2240d0a2567d650dc7832533b4864ac25c
Jack W. <git@jack.cab>
2024-11-24 00:06:11 -0500

    refactor: Add comments to build.ts

--- build.ts
+++ build.ts
@@ -4,9 +4,12 @@ import subsetFont from "subset-font"
 import quotes from "./src/routes/_index/quotes.json"
 
 await $`marko-run build`
+
+// Marko Run outputs a 404/index.html. Cloudflare Pages expects a 404.html. No setting to fix this.
 await $`mv ./dist/404/index.html ./dist/404.html`
 await $`rmdir ./dist/404`
 
+// Dancing Script is used in the Philosophy section. Not all glyphs are used, so subset the font.
 const fonts = new Glob("dancing-script-*.woff*")
 
 for await (const file of fonts.scan({
