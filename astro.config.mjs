986a80dc2fb4690ef68c52c088ade3639a9680b7
Jack W. <git@jack.cab>
2025-01-26 16:51:01 -0500

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from "astro/config"
 import monospaceDark from "./src/monospace-dark.json"
 import monospaceLight from "./src/monospace-light.json"
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jack.cab",
-	markdown: {
-		shikiConfig: {
-			themes: {
-				dark: monospaceDark,
-				light: monospaceLight
-			}
-		}
-	}
+  site: "https://jack.cab",
+
+  markdown: {
+      shikiConfig: {
+          themes: {
+              dark: monospaceDark,
+              light: monospaceLight
+          }
+      }
+	},
+
+  integrations: [sitemap()]
 })
